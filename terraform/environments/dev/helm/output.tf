53c80b3f1ef9d6934e1bf5e98a21494111a303b1
sagarshetty <shettysagar272@gmail.com>
2026-07-15 00:16:10 +0530

    feat: add observability stack with Helm on EKS

--- terraform/environments/dev/helm/output.tf
+++ terraform/environments/dev/helm/output.tf
@@ -9,3 +9,15 @@ output "cluster_autoscaler_status" {
 output "external_secrets_status" {
   value = helm_release.external_secrets.status
 }
+
+output "observability_namespace" {
+  value = kubernetes_namespace.observability.metadata[0].name
+}
+
+output "observability_storage_class" {
+  value = kubernetes_storage_class_v1.observability_gp3.metadata[0].name
+}
+
+output "kube_prometheus_stack_status" {
+  value = helm_release.kube_prometheus_stack.status
+}
